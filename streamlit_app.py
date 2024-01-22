b50210a91a1ad1b88e147dcf691a141168ec2c8b
Jayesh Ahire <112410794+Jayesh-Ah@users.noreply.github.com>
2024-01-22 13:53:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,7 @@
 import streamlit as st
-import spacy
-#nlp = spacy.load("en_core_web_md")
 
-with st.sidebar:  
   st.header("Hi there!")
   st.file_uploader("Upload or drag PDF here", type = "pdf")
 
-def main():
   st.header("PDF Chatbot")
   
-if __name__ =='__main__':
-  main()
