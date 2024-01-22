02150a4f03afea4a3f153d4372dee6fd6af6957b
Jayesh Ahire <112410794+Jayesh-Ah@users.noreply.github.com>
2024-01-22 20:01:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,32 +1,16 @@
 import streamlit as st
-#import fitz
-import numpy as np
-#from spacy.lang.en import English
-#from sklearn.metrics.pairwise import cosine_similarity
-#nlp = spacy.load("en_core_web_sm")
+from chatbot import req_file
 
-def main():
+st.header('''VartaVault Maestro''')
+st.sidebar.header("PDF Chatbot")
+st.sidebar.markdown('''
+## About
+Elevate your document experience with this sleek PDF Chatbot, powered by Python, Spacy, Langchain, and Streamlit. Unleash the synergy of NLP and intuitive design for a seamless interaction journey.''')
+pdf = st.sidebar.file_uploader("Upload or drag PDF here", type = "pdf", accept_multiple_files=True)
+st.sidebar.button("Upload")
 
-  #STREAMLIT APP
-  st.header('''VartaVault Maestro''')
-  st.sidebar.header("PDF Chatbot")
-  st.sidebar.markdown('''
-  ## About
-  Elevate your document experience with this sleek PDF Chatbot, powered by Python, Spacy, Langchain, and Streamlit. Unleash the synergy of NLP and intuitive design for a seamless interaction journey.''')
-  pdf = st.sidebar.file_uploader("Upload or drag PDF here", type = "pdf", accept_multiple_files=True)
-  st.sidebar.button("Upload")
-
-  #EXTRACTING TEXT
-  #def extract(doc):
-  #  file = fitz.open(doc)
-  #  words = ""
-  #  for page_num in range(file.page_count):
-   #   page = doc[page_num]
-  #    words += page.get_text()
-  #  doc.close()
-  #  return words
-  
-
-if __name__ == '__main__':
-  main()
-  
+if st.button("Process"):
+  if pdf:
+    result = req_file(pdf)
+    st.write('### Results:")
+    st.write(result)
