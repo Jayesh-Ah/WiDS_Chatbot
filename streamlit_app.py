66123f02f5cdf2f41325e1fd5026364c4b55661f
Jayesh Ahire <112410794+Jayesh-Ah@users.noreply.github.com>
2024-01-22 13:56:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-  st.header("Hi there!")
-  st.file_uploader("Upload or drag PDF here", type = "pdf")
+  st.sidebar.header("Hi there!")
+  st.sidebar.file_uploader("Upload or drag PDF here", type = "pdf")
 
   st.header("PDF Chatbot")
   
