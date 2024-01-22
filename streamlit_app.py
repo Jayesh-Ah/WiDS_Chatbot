2d6ce2f0fbba3e36259c5daa748562f95abb031d
Jayesh Ahire <112410794+Jayesh-Ah@users.noreply.github.com>
2024-01-22 14:05:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,11 @@
 import streamlit as st
 
+st.header('''## VartaVault Maestro''')
+
 st.sidebar.header("PDF Chatbot")
 st.sidebar.markdown('''
 ## About
 Elevate your document experience with this sleek PDF Chatbot, powered by Python, Spacy, Langchain, and Streamlit. Unleash the synergy of NLP and intuitive design for a seamless interaction journey.''')
 st.sidebar.file_uploader("Upload or drag PDF here", type = "pdf")
 
-st.header('''## VartaVault Maestro''')
   
