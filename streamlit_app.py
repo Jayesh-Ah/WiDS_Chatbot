aa9565dc15d639b7a21211ee288451ad5bb03e5e
Jayesh Ahire <112410794+Jayesh-Ah@users.noreply.github.com>
2024-01-22 19:03:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,14 +5,23 @@ import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 nlp = spacy.load("en_core_web_md")
 
-st.header('''VartaVault Maestro''')
+#EXTRACTING TEXT
+def extract(doc):
+  file = fitz.open(doc)
+  words = ""
+  for page_num in range(file.page_count):
+    page = doc[page_num]
+    words += page.get_text()
+  doc.close()
+  return words
 
+#STREAMLIT APP
+st.header('''VartaVault Maestro''')
 st.sidebar.header("PDF Chatbot")
 st.sidebar.markdown('''
 ## About
 Elevate your document experience with this sleek PDF Chatbot, powered by Python, Spacy, Langchain, and Streamlit. Unleash the synergy of NLP and intuitive design for a seamless interaction journey.''')
-
-st.sidebar.file_uploader("Upload or drag PDF here", type = "pdf", accept_multiple_files=True)
+pdf = st.sidebar.file_uploader("Upload or drag PDF here", type = "pdf", accept_multiple_files=True)
 st.sidebar.button("Upload")
 
   
