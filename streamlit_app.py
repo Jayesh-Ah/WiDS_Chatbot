fb128bf13e6ace1ae9c840789201315616ca9f5b
Jayesh Ahire <112410794+Jayesh-Ah@users.noreply.github.com>
2024-01-22 14:13:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,6 @@ st.sidebar.markdown('''
 ## About
 Elevate your document experience with this sleek PDF Chatbot, powered by Python, Spacy, Langchain, and Streamlit. Unleash the synergy of NLP and intuitive design for a seamless interaction journey.''')
 
-#st.sidebar.add_vertical_space(3)
 st.sidebar.file_uploader("Upload or drag PDF here", type = "pdf")
 st.sidebar.button("Upload")
 
